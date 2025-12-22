f649c3ed0d1fc6df409f781dbbf8a404111aebfc
Everton <evertondiasbrito@gmail.com>
2025-12-22 19:12:36 +0000

    base do projeto app dona

---
+++ main.js
@@ -0,0 +1,53 @@
+const { app , BrowserWindow, Menu } = require('electron');
+
+//* WINDOW CREATION *//
+var mainWindow = null;
+async function createWindow(){
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        
+    });
+    
+    await mainWindow.loadFile("src/pages/editor/index.html");
+
+}
+
+//TEMPLATE MENU*//
+const templateMenu = [
+    {
+        label: 'Arquivo',
+        submenu: [
+            {
+                label: 'Novo Arquivo'
+            },
+            {
+                label: 'Abrir'
+            },
+            {
+                label: 'Salvar'
+            },
+            {
+                label: 'Salvar Como'
+            },
+            {
+                label: 'Sair',
+                role:process.platform === "darwin" ? "close" : 'quit'
+            },
+        ]
+    }
+];
+
+//MENU BUILD*//
+const menu = Menu.buildFromTemplate(templateMenu);
+Menu.setApplicationMenu(menu);
+
+// ON READY*//
+app.whenReady().then(createWindow);
+
+//ACTIVAR APLLE *//
+app.on('activate',()=>{
+    if(BrowserWindow.getAllWindows().length === 0){
+        createWindow();
+    }
+}); 
